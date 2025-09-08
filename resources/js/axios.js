3a6d393748f01f65f24d160a89c97df5c65e3320
Panwar Rahul <panwar@egrowth-inc.com>
2025-09-08 12:44:10 +0900

    android issue resolved

--- resources/js/axios.js
+++ resources/js/axios.js
@@ -1,5 +1,13 @@
 import axios from 'axios'
 
+// Set base URL for API calls
+// In development (web), use relative URLs
+// In production (mobile), use the full URL
+const isMobile = window.Capacitor && window.Capacitor.isNativePlatform()
+const baseURL = isMobile ? 'http://10.0.2.2:8001' : ''
+
+axios.defaults.baseURL = baseURL
+
 // Set default headers
 axios.defaults.headers.common['Accept'] = 'application/json'
 axios.defaults.headers.common['Content-Type'] = 'application/json'
